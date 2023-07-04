c6d4869cfb2affd73d197449fd59d9204405c521
Andreas Möller <am@localheinz.com>
2023-07-04 12:05:35 +0200

    Enhancement: Implement Content as value object

---
+++ test/Unit/ContentTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2020-2023 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/front-matter
+ */
+
+use Ergebnis\DataProvider;
+use Ergebnis\FrontMatter\Content;
+use PHPUnit\Framework;
+
+#[Framework\Attributes\CoversClass(Content::class)]
+final class ContentTest extends Framework\TestCase
+{
+    #[Framework\Attributes\DataProviderExternal(DataProvider\StringProvider::class, 'arbitrary')]
+    public function testFromStringReturnsContent(string $value): void
+    {
+        $content = Content::fromString($value);
+
+        self::assertSame($value, $content->toString());
+    }
+}
